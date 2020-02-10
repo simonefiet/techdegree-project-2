f87e34e895be779e404c671b8a23ba57b8194ea6
Simone <sft@rentsafe.dk>
2020-02-10 10:50:20 +0100

    Remove score from index

--- index.php
+++ index.php
@@ -6,8 +6,7 @@
                 <?php echo feedback(); ?>
 
             </div>
-        <?php } 
-        echo $score; ?>
+        <?php } ?>
         <p class="breadcrumbs">
             <!-- Show which question they are on -->
             <?php echo 'Question ' . $question . ' of ' .  $totalQuestions; ?>
